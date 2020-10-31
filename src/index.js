3132a8aa1e3ce396f591ed99a7580cd7fb0c1a64
Antonio Matheus <antonimathsilva@gmail.com>
2020-10-31 08:47:54 -0300

    Added eslint verification

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
